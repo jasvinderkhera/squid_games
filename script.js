da3b65d0eb826955b8e905aef78bd07eea011af9
jasvinderkhera <ankitkhera15@gmail.com>
2025-01-26 14:03:24 +0530

    menu page added

--- script.js
+++ script.js
@@ -38,3 +38,56 @@ tl.to('.bottom-section',{
         scrub: true, // Optional: smooth the opacity change with the scroll
     }
 })
+gsap.from('.navBtn',{
+    opacity:0,
+    scale:0,
+    duration:4,
+    delay:1,
+    stagger:0.25,
+    scrollTrigger: {
+        trigger: ".bottom-section",
+        scroller: "body", // or your custom container like ".my-container"
+        start: "bottom 20%", // Make sure opacity starts as soon as the section hits the viewport
+        end: "top 0", // Optional: adjust how long opacity change lasts
+        // markers: true, // Set to true for debugging
+        scrub: true, // Optional: smooth the opacity change with the scroll
+    }
+})
+
+let menu = gsap.timeline()
+
+
+menu.from('.menu-page',{
+    transform:"translateY(-100%)",
+    opacity:0,
+    delay:0.1,
+    duration:0.6
+})
+menu.to('.menu',{
+    y:-300,
+    opacity:0,
+    duration:0.1,
+})
+menu.from('.menuItems',{
+    y:-100,
+    opacity:0,
+    delay:0.15,
+    duration:0.1,
+    stagger:0.05,
+})
+
+
+menu.pause()
+
+let menuBtn = document.querySelector('.menu')
+
+menuBtn.addEventListener('click',function(){
+    menu.play();
+    document.body.style.overflow = 'hidden';
+})
+
+function closeBtn(){
+    console.log("working")
+    document.body.style.overflow = '';
+    menu.reverse()
+}
